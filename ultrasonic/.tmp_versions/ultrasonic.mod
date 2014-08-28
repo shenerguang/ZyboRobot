b8d6c56212a778f4c6ce65dd16d07d9f4e1467f5
root <root@seg-VirtualBox.(none)>
2014-08-28 21:45:34 +0800

    del

--- ultrasonic/.tmp_versions/ultrasonic.mod
+++
@@ -1,2 +0,0 @@
-/root/build/zrobot_v1-master/sw/driver/ultrasonic/ultrasonic.ko
-/root/build/zrobot_v1-master/sw/driver/ultrasonic/ultrasonic.o
--- ultrasonic/.ultrasonic.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/build/zrobot_v1-master/sw/driver/ultrasonic/ultrasonic.ko := arm-xilinx-linux-gnueabi-ld -EL -r  -T /root/zybo_gpio/linux-digilent/scripts/module-common.lds --build-id  -o /root/build/zrobot_v1-master/sw/driver/ultrasonic/ultrasonic.ko /root/build/zrobot_v1-master/sw/driver/ultrasonic/ultrasonic.o /root/build/zrobot_v1-master/sw/driver/ultrasonic/ultrasonic.mod.o
